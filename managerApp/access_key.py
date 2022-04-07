630d945944421c9d55b8bcc47281bc0e2cc7ee42
Josh Abraham <josh.m.n.abraham@gmail.com>
2022-04-07 00:46:51 -0400

    Update access_key.py

--- managerApp/access_key.py
+++ managerApp/access_key.py
@@ -1,4 +1,4 @@
 aws_config = {
-    'aws_access_key_id': 'AKIA3U4U6D42PLQZVFOX',
-    'aws_secret_access_key': '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'
+    'aws_access_key_id': '',
+    'aws_secret_access_key': ''
 }
